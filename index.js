45427b779ccf3c3532b2dfbf299d39c5d4f5e137
Alice Freda <alicefreda@gmail.com>
2020-05-04 14:38:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
+    h2.innerHTML = `${book.name}`
     main.appendChild(h2)
   })
 }
